ba353185cbd9435b67079dab0a74d809d85d521d
AndresBaron21 <AndresBaron2018@outlook.com>
2023-06-08 10:26:08 -0500

    Test 1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,7 @@ class HomeController extends Controller
 {
     public function index()
     {
+        phpinfo();
         // Fetch humidity data from the API
         $humidityData = $this->fetchHumidityData();
         
